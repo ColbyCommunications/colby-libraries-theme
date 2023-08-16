bec96ce6eb1de087e5090fe6488df0277d2d5b52
Brandon K. Waltz <bwaltz6@gmail.com>
2023-08-16 12:22:04 -0400

    search

--- src/components.js
+++ src/components.js
@@ -1,5 +1,7 @@
 import LibrariesSearch from './twig/components/libraries-search/LibrariesSearch.vue';
+import Search from './twig/components/search/Search.vue';
 
 export default {
     LibrariesSearch,
+    Search,
 };
