6cc64f6e2f4459cb91be498e78a361f24d2a39e3
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2024-01-18 13:13:33 -0500

    modify search widget

--- src/twig/components/libraries-search/search-tabs/SpecialCollectionsAndArchives.vue
+++ src/twig/components/libraries-search/search-tabs/SpecialCollectionsAndArchives.vue
@@ -2,50 +2,113 @@
     <div>
         <form
             class="mb-[3px]"
-            id="sc-search"
-            name="scSearchForm"
+            id="pc-search"
+            name="pcSearchForm"
             action="https://librarysearch.colby.edu/discovery/search"
             accept-charset="utf-8"
             method="GET"
-            @submit.prevent="handleSCSearch"
+            @submit.prevent="handlePCSearch"
+            v-show="collectionSelection === 'physical'"
         >
-            <div v-show="collectionSelection === 'physical'" class="search-box">
+            <div class="search-box">
                 <input
                     type="text"
                     placeholder="Search for physical materials in our special collections and archives"
                     class="summon-search-field search-bar-full"
                     autocomplete="off"
-                    v-model="scQuery"
+                    v-model="pcQuery"
                 />
-                <input type="hidden" name="query" :value="'any,contains,' + scQuery" />
+                <input type="hidden" name="query" :value="'any,contains,' + pcQuery" />
                 <input type="hidden" name="vid" value="01CBB_CCLIBRAR:COLBY" />
                 <input type="hidden" name="search_scope" value="SCA" />
-                <input type="image" @click.prevent="handleSCSearch" :src="search" alt="search" />
-            </div>
-            <div class="sc-btn-container text-12 sm:text-[11px] md:text-12">
-                <input
-                    id="physical-collections-btn"
-                    type="radio"
-                    value="physical"
-                    name="special-collections"
-                    checked="checked"
-                    autocomplete="off"
-                    class="mr-2 hover:cursor-pointer"
-                    v-model="collectionSelection"
-                />
-                <label for="physical-collections-btn" class="mr-4">Physical Collections</label>
-                <input
-                    id="digital-collections-btn"
-                    type="radio"
-                    value="digital"
-                    name="special-collections"
-                    autocomplete="off"
-                    class="mr-2 hover:cursor-pointer"
-                    v-model="collectionSelection"
-                />
-                <label for="digital-collections-btn">Digital Collections</label>
+                <input type="image" @click.prevent="handlePCSearch" :src="search" alt="search" />
             </div>
         </form>
+        <form
+            class="mb-[3px]"
+            id="sc-search"
+            name="scSearchForm"
+            :action="
+                selectedSearchScope === 'digitalCommons'
+                    ? 'https://digitalcommons.colby.edu/do/search/'
+                    : 'https://www.jstor.org/action/doBasicSearch'
+            "
+            accept-charset="utf-8"
+            method="GET"
+            @submit.prevent="handleSCSearch"
+            v-show="collectionSelection === 'digital'"
+        >
+            <div class="search-box">
+                <input
+                    type="text"
+                    :placeholder="placeholderText"
+                    class="summon-search-field search-bar"
+                    autocomplete="off"
+                    v-model="scQuery"
+                />
+                <input
+                    type="hidden"
+                    :name="selectedSearchScope === 'digitalCommons' ? 'q' : 'Query'"
+                    :value="scQuery"
+                />
+                <input
+                    v-if="selectedSearchScope === 'digitalCommons'"
+                    type="hidden"
+                    name="start"
+                    value="0"
+                />
+                <input
+                    v-if="selectedSearchScope === 'digitalCommons'"
+                    type="hidden"
+                    name="context"
+                    value="624264"
+                />
+                <input
+                    v-if="selectedSearchScope === 'digitalCommons'"
+                    type="hidden"
+                    name="facet"
+                    value=""
+                />
+                <input
+                    v-if="selectedSearchScope === 'JSTOR'"
+                    type="hidden"
+                    name="scope"
+                    value="eyJpZCI6ICIxMDA0NTgwNzEiLCAicGFnZU5hbWUiOiAiQ29sYnkgQ29sbGVnZSIsICJwYWdlVXJsIjogIi9zaXRlL2NvbGJ5Y29sbGVnZWxpYnJhcmllcy8iLCAidHlwZSI6ICJwb3J0YWwifQ%3D%3D"
+                />
+                <div class="drop-down">
+                    <div class="flex items-center">
+                        <select v-model="selectedSearchScope" class="outline-offset-8">
+                            <option value="digitalCommons">Digital Commons</option>
+                            <option value="JSTOR">JSTOR</option>
+                        </select>
+                    </div>
+                </div>
+                <input type="image" @click.prevent="handleSCSearch" :src="search" alt="search" />
+            </div>
+        </form>
+        <div class="mb-2 sc-btn-container text-12 sm:text-[11px] md:text-12">
+            <input
+                id="physical-collections-btn"
+                type="radio"
+                value="physical"
+                name="special-collections"
+                checked="checked"
+                autocomplete="off"
+                class="mr-2 hover:cursor-pointer"
+                v-model="collectionSelection"
+            />
+            <label for="physical-collections-btn" class="mr-4">Physical Collections</label>
+            <input
+                id="digital-collections-btn"
+                type="radio"
+                value="digital"
+                name="special-collections"
+                autocomplete="off"
+                class="mr-2 hover:cursor-pointer"
+                v-model="collectionSelection"
+            />
+            <label for="digital-collections-btn">Digital Collections</label>
+        </div>
         <section v-show="collectionSelection === 'physical'">
             <div class="text-12 sm:text-[11px] md:text-12">
                 <p class="mb-2">
@@ -71,60 +134,43 @@
         </section>
         <section v-show="collectionSelection === 'digital'">
             <div class="text-12 sm:text-[11px] md:text-12">
-                <p class="mb-2">
-                    Explore Colby’s <strong>digital</strong> Special Collections & Archives
-                    collections which include both culturally significant materials and Colby
-                    documents and memorabilia from the 13th through the 21st centuries.
+                <p>
+                    Explore our digital special collections and archives, including culturally
+                    significant materials and Colby documents and memorabilia from the 13th through
+                    the 21st centuries.
                 </p>
-                <ul class="pl-6 list-disc">
-                    <li>
-                        <p>
-                            <a
-                                class="text-linkBlue no-underline"
-                                href="https://digitalcommons.colby.edu/"
-                                >Digital Commons</a
-                            >: Explore senior theses, college publications, yearbooks, and more.
-                        </p>
-                    </li>
-                    <li>
-                        <p>
-                            <a
-                                class="text-linkBlue no-underline"
-                                href="https://library.artstor.org/#/browse/institution"
-                                >Artstor</a
-                            >: Discover archival photo collections, unique materials and items from
-                            the Libraries’ special collections and beyond.
-                        </p>
-                    </li>
-                    <li>
-                        <p>
-                            Special Collections and Archives’
-                            <a
-                                class="text-linkBlue no-underline"
-                                href="https://web.colby.edu/csc-home/"
-                                >web portal</a
-                            >
-                            contains digital exhibits displaying curated collection holdings
-                            selections.
-                        </p>
-                    </li>
-                </ul>
             </div>
         </section>
     </div>
 </template>
 <script setup>
-    import { ref } from 'vue';
+    import { ref, computed } from 'vue';
     import search from '../img/search.svg';
 
     // set the default selection to physical collections
     const collectionSelection = ref('physical');
 
+    const pcQuery = ref(null);
     const scQuery = ref(null);
+
+    const handlePCSearch = () => {
+        // Submit the form
+        document.pcSearchForm.submit();
+    };
 
     const handleSCSearch = () => {
         // Submit the form
         document.scSearchForm.submit();
     };
+
+    const selectedSearchScope = ref('digitalCommons');
+
+    const placeholderText = computed(() => {
+        if (selectedSearchScope.value === 'digitalCommons') {
+            return 'Explore senior theses, college publications, yearbooks, and more.';
+        } else if (selectedSearchScope.value === 'JSTOR') {
+            return 'Search our archival photo collections and other unique materials.';
+        }
+    });
 </script>
 <style scoped></style>
